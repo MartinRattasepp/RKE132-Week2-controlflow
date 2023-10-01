4cb5cd8401fdaf5f8937ef482067c8495beb97ff
Martin R <martin.rattasepp@tktk.ee>
2023-10-01 23:33:25 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Martin!");
+
+//rakendus küsib kasutajalt valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"welcome, Mr. (kasutajanimi) / "welcome, Ms. (kasutaja perekonnanimi)"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+
+Console.WriteLine($"Welcome, {userGender}");
---
+++ UserAgeValidation2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasjutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. (kasutaja perekonnanimi)" / "Welcome, Ms. (kasutaja perekonnanimi)"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+
+Console.WriteLine("please enter your lastname:");
+string UserLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {UserLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"welcome, Ms.{UserLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {UserLastName}!");
+}
+
+     
